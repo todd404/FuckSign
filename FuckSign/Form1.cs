216a66f56c302a8538b9695b5d6e006c1aa18bfe
toddwu <wutob12450@outlook.com>
2021-04-18 10:51:46 +0800

    添加项目文件。

---
+++ FuckSign/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TrotiNet;
+
+namespace FuckSign
+{
+    public class TransparentProxy : ProxyLogic
+    {
+        public TransparentProxy(HttpSocket httpSocket)
+            : base(httpSocket) { }
+
+        //这个类的构造器
+        static new public TransparentProxy CreateProxy(HttpSocket httpSocket)
+        {
+            return new TransparentProxy(httpSocket);
+        }
+
+        protected override void OnReceiveRequest()
+        {
+            Console.WriteLine("-> " + RequestLine);
+        }
+
+        protected override void OnReceiveResponse()
+        {
+            Console.WriteLine("<- " + ResponseStatusLine);
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private TcpServer Server;
+        private Process process;
+        public Form1()
+        {
+            InitializeComponent();
+
+            int port = 1203;
+            bool bUseIPv6 = false;
+            Server = new TcpServer(port, bUseIPv6);
+            Server.Start(TransparentProxy.CreateProxy);
+
+            Server.InitListenFinished.WaitOne();
+            if (Server.InitListenException != null)
+                throw Server.InitListenException;
+
+            var frp = new Frp();
+
+            process = new Process();
+            process.StartInfo.FileName = @"C:\Users\Lenovo\source\repos\FuckSign\frp\frpc.exe";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.Exited += new EventHandler(p_Exited);
+            process.OutputDataReceived += new DataReceivedEventHandler(p_OutputDataReceived);
+            process.Start();
+
+            process.BeginOutputReadLine();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Server.Stop();
+            process.Kill();
+        }
+
+        void p_OutputDataReceived(Object sender, DataReceivedEventArgs e)
+        {
+            //这里是正常的输出
+            Console.WriteLine(e.Data);
+
+        }
+
+        void p_Exited(Object sender, EventArgs e)
+        {
+            Console.WriteLine("finish");
+        }
+    }
+}
---
+++ FuckSign/Frp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using IniParser;
+using IniParser.Model;
+
+namespace FuckSign
+{
+    class Frp
+    {
+        private const string frpPath = @".\frp\frpc.exe";
+        private const string iniPath = @".\frpc.ini";
+
+        static Frp()
+        {
+            if (!File.Exists(iniPath))
+            {
+                File.Create(iniPath).Close();
+                
+                var parser = new FileIniDataParser();
+                IniData data = new IniData();
+
+                data.Sections.AddSection("common");
+                data["common"].AddKey("server_addr", "www.7066.site");
+                data["common"].AddKey("server_port", "7000");
+
+                data.Sections.AddSection("proxy");
+                data["proxy"].AddKey("type", "tcp");
+                data["proxy"].AddKey("local_ip", "127.0.0.1");
+                data["proxy"].AddKey("local_port", "1203");
+
+                parser.WriteFile(iniPath, data);
+            }
+        }
+
+        public void setRemotePort(int port)
+        {
+            var parser = new FileIniDataParser();
+            IniData data = parser.ReadFile(iniPath);
+
+            data["proxy"]["remote_port"] = port.ToString();
+        }
+    }
+}
